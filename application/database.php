9fc0675bc671204b184526fedff77b59ef8978e7
zhouzzz <1025713522@qq.com>
2020-03-10 12:08:09 +0800

    fix database config

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type' => 'mysql',
     // 服务器地址
-    'hostname' => '127.0.0.1',
+    'hostname' => '47.101.144.228',
     // 数据库名
     'database' => 'zerg',
     // 用户名
-    'username' => 'root',
+    'username' => 'zhou',
     // 密码
-    'password' => '123',
+    'password' => 'zz445566',
     // 端口
     'hostport' => '3306',
     // 连接dsn
